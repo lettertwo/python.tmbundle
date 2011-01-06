2ead6e9564fbfe140a8d5278ab237a78f7983b05
Eric Eldredge <lettertwo@gmail.com>
2011-01-06 18:46:03 -0500

    Refined rope implementation. Should do a better autocomplete job now

--- Support/bin/complete.py
+++ Support/bin/complete.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # encoding: utf-8
+import sys
 import textmate
 from textmate import ui
 
@@ -13,37 +14,86 @@ try:
 except:
     textmate.exit_show_tool_tip('Rope module not found!')
 
+# def main():
+#     try:
+#         # TODO: Determine if this is necessary. Can we still provide basic completion in a 'standalone' file?
+#         if textmate.PROJECT_DIRECTORY is None:
+#             textmate.exit_show_tool_tip('No completions.')
+#             
+#         current_word = textmate.current_word(r"[\w_]*", 'both')
+# 
+#         f = open(textmate.FILEPATH, 'r')
+#         source = f.read()
+#         f.close()
+# 
+#         project = Project(textmate.PROJECT_DIRECTORY)
+#         resource = project.get_resource(textmate.FILEPATH.replace(textmate.PROJECT_DIRECTORY, '')[1:])
+#         caret_index = source.find(textmate.CURRENT_LINE) + textmate.LINE_INDEX
+#         
+#         completions = codeassist.code_assist(project, source, caret_index, resource)
+#         if len(completions) == 1:
+#             selection = completions[0].name
+#             textmate.exit_insert_text(selection.replace(current_word, '', 1))
+#         elif len(completions):
+#             completions = codeassist.sorted_proposals(completions)
+#             completions = [{'display': completion.name, 'insert': completion.name} for completion in completions]
+#             selection = ui.complete(completions, {'initial_filter': current_word, 'extra_chars': "_"})
+# 
+#         else:
+#             selection = None
+#             textmate.exit_show_tool_tip('No completions.')
+# 
+#     except Exception as e:
+#         textmate.exit_show_tool_tip('ERROR %s: %s' % (type(e), e))
+
+
+
+
+
+
 def main():
+    # TODO: Determine if this is necessary. Can we still provide basic completion in a 'standalone' file?
+    if textmate.PROJECT_DIRECTORY is None:
+        textmate.exit_show_tool_tip('No completions.')
+
+    source = sys.stdin.read()
+
+    #from rope.contrib import autoimport
+    project = Project(textmate.PROJECT_DIRECTORY)
+    #autoimport = autoimport.AutoImport(project)
+    resource = project.get_resource(textmate.FILEPATH.replace(textmate.PROJECT_DIRECTORY, '')[1:])
+    #project.validate(self.project_path)
+    caret_index = source.find(textmate.CURRENT_LINE) + textmate.LINE_INDEX
+
+    current_word = textmate.current_word(r"[\w_]*", 'both')
+
+    proposals = codeassist.code_assist(project, source, caret_index, resource)
+
     try:
-        # TODO: Determine if this is necessary. Can we still provide basic completion in a 'standalone' file?
-        if textmate.PROJECT_DIRECTORY is None:
-            textmate.exit_show_tool_tip('No completions.')
-            
-        current_word = textmate.current_word(r"[\w_]*", 'both')
+        if len(proposals) == 0:
+            textmate.exit_show_tool_tip("No completions.")
+    except:
+        textmate.exit_show_tool_tip("No completions.")
+    if len(proposals) == 1:
+        textmate.exit_insert_text(proposals[0].name.replace(current_word, '', 1))
+    else:
+        proposals = codeassist.sorted_proposals(proposals)
+        #autoimport.generate_cache()
+        #autoimport.generate_modules_cache(modules)
+        #project.pycore.analyze_module(resource)
+        names = [proposal.name for proposal in proposals]
+        #if self.starting.strip() and '.' not in self.expression:
+        #        import_assists = self.autoimport.import_assist(self.starting)
+        #        names.extend(x[0] + ' : ' + x[1] for x in import_assists)
 
-        f = open(textmate.FILEPATH, 'r')
-        source = f.read()
-        f.close()
+        #plist = "{ menuItems=(%s);}"
+        selection = ui.complete(names)
 
-        project = Project(textmate.PROJECT_DIRECTORY)
-        resource = project.get_resource(textmate.FILEPATH.replace(textmate.PROJECT_DIRECTORY, '')[1:])
-        caret_index = source.find(textmate.CURRENT_LINE) + textmate.LINE_INDEX
-        
-        completions = codeassist.code_assist(project, source, caret_index, resource)
-        if len(completions) == 1:
-            selection = completions[0].name
-            textmate.exit_insert_text(selection.replace(current_word, '', 1))
-        elif len(completions):
-            completions = codeassist.sorted_proposals(completions)
-            completions = [{'display': completion.name, 'insert': completion.name} for completion in completions]
-            selection = ui.complete(completions, {'initial_filter': current_word, 'extra_chars': "_"})
 
-        else:
-            selection = None
-            textmate.exit_show_tool_tip('No completions.')
 
-    except Exception as e:
-        textmate.exit_show_tool_tip('ERROR %s: %s' % (type(e), e))
+
+
+
 
 
 # def goto_definition(self):
