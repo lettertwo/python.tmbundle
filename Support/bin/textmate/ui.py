a0bc7d39bfd73649b321b46b55cab3c07599ff52
Eric Eldredge <lettertwo@gmail.com>
2011-01-07 14:16:38 -0500

    Execute Dialog2 through the shell instead of directly. This seems to solve the 'alreadyTyped' issues where completed portions were being duplicated on insert

--- Support/bin/textmate/ui.py
+++ Support/bin/textmate/ui.py
@@ -1,6 +1,8 @@
 import re
+import tempfile
+import plistlib
+import subprocess
 import textmate
-import tempfile, plistlib, subprocess, sys, os
 
 # TODO: rewrite this as pydoc
 # Interactive Code Completion Selector
@@ -23,87 +25,55 @@ import tempfile, plistlib, subprocess, sys, os
 # * <tt>:case_insensitive</tt>  -- ignore case when filtering
 # * <tt>:static_prefix</tt>     -- a prefix which is used when filtering suggestions.
 # * <tt>:initial_filter</tt>    -- defaults to the current word
+# TODO: implement support for images
 # * <tt>:images</tt>            -- a +Hash+ of image names to paths
 # 
-# If a block is given, the selected item from the +choices+ array will be yielded
-# (with a new key +index+ added, which is the index of the +choice+ into the +choices+ array)
-# and the result of the block inserted as a snippet
-def complete(choices, options = {}, block = None):
+def complete(choices, options = {}):
+    
     if '2' not in textmate.DIALOG:
         raise 'Dialog2 not found.'
     
     characters = 'a-zA-Z0-9'
     if 'extra_chars' in options:
         characters += re.escape(options['extra_chars'])
-        
+    
     if 'initial_filter' not in options:
         options['initial_filter'] = textmate.current_word(characters, "left")
 
-    command = [textmate.DIALOG, "popup"]
-    if "initial_filter" in options:
+    command = [textmate.DIALOG, "popup", "--returnChoice"]
+    if "initial_filter" in options and options['initial_filter']:
         command.append("--alreadyTyped %s" % textmate.sh_escape(options["initial_filter"]))
-    if "static_prefix" in options:
+    if "static_prefix" in options and options['static_prefix']:
         command.append("--staticPrefix %s" % textmate.sh_escape(options["static_prefix"]))
-    if "extra_chars" in options:
+    if "extra_chars" in options and options['extra_chars']:
         command.append("--additionalWordCharacters %s" % textmate.sh_escape(options['extra_chars']))
-    if "case_insensitive" in options:
+    if "case_insensitive" in options and options['case_insensitive']:
         command.append("--caseInsensitive")
 
     def formalize(choice):
         try:
             choice['display']
-            choice['insert']
             return choice
         except (KeyError, IndexError, TypeError):
-            return {'display': choice, 'insert' : choice}
-    
+            return {'display': choice}
+
     choices = [formalize(choice) for choice in choices]
     
     plist = {'suggestions': choices}
-
-    result = None
 
     try:
         f = tempfile.NamedTemporaryFile()
         plistlib.writePlist(plist, f)
         f.seek(0)
+        
+        command = ' '.join(command).strip()
 
-        # command = [textmate.DIALOG, 'popup', '--suggestions', '({display = test; insert = tesinsert; }, {display = testinger; })']
-        command.append('--returnChoice')
-
-        DEVNULL = os.open(os.devnull, os.O_RDWR)
-        process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=DEVNULL, stderr=DEVNULL)
+        process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
         process.stdin.write(f.read())
+        process.stdin.close()
+        f.close()
 
-        
-        # TODO: How to prevent the command from inserting the result??
-        
-
-        # # Deserialize the result dictionary
-        # resultDict = {}
-        # lines = result.split('\n')
-        # for line in lines:
-        #     if '=' in line:
-        #         key, value = line.split('=')  
-        #         resultDict[key.strip()] = re.sub(';', '', value.strip())
-        #     
-        # result = resultDict
     except Exception as e:
         textmate.exit_show_tool_tip('ERROR: %s' % e)
     finally:
         f.close()
-    
-
-    # Use a default block if none was provided
-    # if not block:
-    #     block = lambda choice:\
-    #         choice['insert'] if choice else None
-
-    # The block should return the text to insert as a snippet
-    # to_insert = block.__call__(result)
-    
-    # return to_insert
-    # if to_insert:
-    #     textmate.sh("%s x-insert --snippet %s" % (textmate.DIALOG,
-    #         textmate.sh_escape(to_insert)))
-    # 
